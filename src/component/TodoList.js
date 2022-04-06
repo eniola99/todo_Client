96a077add9abd718fc966a48caf2105e91719d33
eniola99 <qudusnurudeen9@gmail.com>
2022-04-06 14:03:43 +0100

    message

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,67 +1,34 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+
+import React, {useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Todo = props => (
-    <tr>
-        <td>{props.todo.todo_description}</td>
-        <td>{props.todo.todo_responsible}</td>
-        <td>{props.todo.todo_priority}</td>
-        <td>
-            
-            <Link to={"/edit/"+props.todo._id}>Edit</Link>
-        </td>
-        <td>
-            <Link to={'/edit/'+props.todo._id}>Print</Link>
-        </td>
-    </tr>
-)
+const TodoList = () => {
+    const [ task, setTask ] = useState([])
 
-
-export default class TodoList extends Component{
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            todos: []
-        }
-    }
-
-    componentDidMount() {
-        axios.get('https://qudustodo.herokuapp.com/todo/find')
-        .then(response => {
-            this.setState({
-                todos: response.data
-            })
-        }).catch(err => {
-            console.log(err)
+    const handleFetch = () => {
+        axios.get('https://qudustodo.herokuapp.com/todo/find').then(res => {
+            setTask(res.data)
         })
     }
 
-    todoList() {
-        return this.state.todos.map((currentTodo, i) => {
-            return <Todo todo={currentTodo} key={i} />
-        })
-    }
-    render() {
-        return(
-            <div>
-                <h3>Todo List</h3>
+    useEffect(() => {
+        handleFetch()
+    }, [])
+    return (
+        <div>
+             <h3>Todo List</h3>
                 <table style={{marginTop: 20}} className='table table-striped'>
-                <thead>
-                    <tr>
-                        <th>Description</th>
-                        <th>Responsible</th>
-                        <th>Priority</th>
-                        <th>Action</th>
-                        <th>Print</th>
-                    </tr>
-                </thead>
                     <tbody>
-                        { this.todoList() }
+                    {task.map((activeTodo, i) => (
+                        <>
+                        <p>{activeTodo.newTask}</p>
+                        <p>{activeTodo.description}</p>
+                        </>
+                    ))}
                     </tbody>
                 </table>
-            </div>
-        )
-    }
+        </div>
+    )
 }
+
+export default TodoList
